109f38ad5fdf3a7c03620693df07b605d29b08ab
Nicolo' Ferri <irr3f92@gmail.com>
2023-11-07 10:18:37 +0100

    Create a program that accepts user input. Ask the user for their name, and then print it out in the following format: "Hello, ".

---
+++ Esercizio2/Esercizio2/Program.cs
@@ -0,0 +1,5 @@
+﻿//Create a program that accepts user input. Ask the user for their name.
+//, and then print it out in the following format: "Hello, ".
+Console.WriteLine("Immetti il tuo nome :");
+    String nome = Console.ReadLine();
+Console.WriteLine("Ciao , " + nome);
